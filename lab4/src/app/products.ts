e5bfa3c201a094a7ef1310434f92a02ef98013b3
askhat-king <78610124+askhat-king@users.noreply.github.com>
2021-02-24 15:12:08 +0600

    Add files via upload

---
+++ lab4/src/app/products.ts
@@ -0,0 +1,124 @@
+export const products = [
+  {
+    id: 1,
+    name: "Microsoft Surface Headphones 2 - Light Gray",
+    price: "$245.36",
+    description:
+      "Experience your favorite music and shows like never before. Designed for all-day comfort, improved sound quality, and more battery life, Surface Headphones 2 features voice, touch and fingertip controls with 13 levels of active noise cancellation.",
+    rating:"4.5/5",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61v5xP47SbL._AC_SL1500_.jpg",
+    link:
+      "https://www.amazon.com/dp/B086Q9YGD3/ref=twister_B088B22VLB?_encoding=UTF8&psc=1"
+  },
+  {
+    id: 2,
+    name: "Sony WH-1000XM4",
+    price: "$348.00",
+    description:"Sony’s intelligent industry-leading noise canceling headphones with premium sound elevate your listening experience with the ability to personalize and control everything you hear.",
+    rating:"4.7/5",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71o8Q5XJS5L._AC_SL1500_.jpg",
+    link:
+      "https://www.amazon.com/Sony-WH-1000XM4-Canceling-Headphones-phone-call/dp/B0863TXGM3/ref=sr_1_1?crid=3QSOYN34A5JAQ&dchild=1&keywords=sony+mx4&qid=1614148885&sprefix=sony+mx%2Caps%2C348&sr=8-1"
+  },
+  {
+    id: 3,
+    name: "Beats Solo Pro",
+    price: '$299.95',
+    description: "Experience powerful, balanced sound with Solo Pro wireless on-ear headphones featuring Active Noise Cancelling",
+    rating:"4.7/5",
+    img:"https://images-na.ssl-images-amazon.com/images/I/61w%2BedOrp9L._AC_SL1500_.jpg",
+    link: "https://www.amazon.com/Beats-Wireless-Noise-Cancelling-Headphones-Bluetooth/dp/B07YVYZLC8/ref=sr_1_8?dchild=1&keywords=beats%2Bby%2Bdre&qid=1614152051&sr=8-8&th=1"
+  },
+  {
+    id: 4,
+    name: "Bose QuietComfort 35 II",
+    price: "$299.00",
+    description:
+      "QuietComfort 35 wireless headphones II are engineered with renowned noise cancellation. With the Google Assistant and Amazon Alexa built-in, you have instant access to millions of songs, playlists and more—hands free.* Simply choose your voice assistant and ask away.",
+    rating:"4.7/5",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81%2BjNVOUsJL._AC_SL1500_.jpg",
+    link:
+      "https://www.amazon.com/Bose-QuietComfort-Wireless-Headphones-Cancelling/dp/B0756CYWWD/ref=sr_1_3?crid=33YKGPOX1Z35R&dchild=1&keywords=headphones&qid=1614152319&refinements=p_n_feature_two_browse-bin%3A509316&rnid=2266979011&s=aht&sprefix=headp%2Caps%2C335&sr=1-3"
+  },
+  {
+    id: 5,
+    name: "Sony Noise Cancelling Headphones WHCH710N",
+    price: "$198.00",
+    description:
+      "Cancel out the world with the long-lasting noise cancellation performance of the WH-CH710N wireless headphones Dual Noise Sensor technology automatically senses your environment to deliver an amazing sound experience With up to 35 hours of battery life quick charging and access to your smartphone’s voice assistant the WH-CH710N are sure to be a must-have for your travels.",
+    rating:"4.4/5",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51OF7sr7e6L._AC_SL1200_.jpg",
+    link:
+      "https://www.amazon.com/Sony-Noise-Cancelling-Headphones-WHCH710N/dp/B085RNVJ3P/ref=sr_1_5?crid=33YKGPOX1Z35R&dchild=1&keywords=headphones&qid=1614152319&refinements=p_n_feature_two_browse-bin%3A509316&rnid=2266979011&s=aht&sprefix=headp%2Caps%2C335&sr=1-5"
+  },
+  {
+    id: 6,
+    name: "Bose Noise Cancelling Wireless Bluetooth Headphones 700",
+    price: "$379.00",
+    description:
+      "Bose Noise cancelling headphones 700 have redefined what wireless headphones can do. Keep your head up to the world with easy access to voice assistants — perfect for music, navigation, weather, and more. Confidently take calls or speak to Alexa anywhere. An unrivaled four microphone system picks up and isolates your voice while cancelling the noise around you. With these smart headphones, Bose has even improved on what it’s most known for. Eleven levels of noise cancellation, let you truly personalize your environment. Set it low to let more of the world In, somewhere in the middle, or turn it all the way up to block out the noisy world around you.",
+    rating:"4.5/5",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51ntWa1Q0sL._AC_SL1200_.jpg",
+    link:
+      "https://www.amazon.com/Bose-Cancelling-Wireless-Bluetooth-Headphones/dp/B07Q9MJKBV/ref=sr_1_4?crid=33YKGPOX1Z35R&dchild=1&keywords=headphones&qid=1614152319&refinements=p_n_feature_two_browse-bin%3A509316&rnid=2266979011&s=aht&sprefix=headp%2Caps%2C335&sr=1-4"
+  },
+  {
+    id: 7,
+    name: "Bose SoundLink Around Ear Wireless Headphones II - Black",
+    price: "$229.00",
+    description:
+      "Enjoy a better wireless experience with Bose sound link Around Ear wireless headphones II. Exclusive technology delivers deep, immersive sound at any volume, making them the best sounding wireless headphones available. A dual microphone system rejects noise and wind so you'll be heard loud and clear. And enhanced side tone makes your voice sound more natural. Sound link wireless headphones use the latest Bluetooth technology so you can easily connect to your mobile devices with seamless audio/video sync and switch between two devices. For NFC enabled devices, you can simply tap the right ear cup for quick pairing. A rechargeable lithium ion battery lets you listen for up to 15 hours and gives you up to 2 hours of play time with a 15 minute charge. Effortless touch controls allow for simple command of your music and calls. And voice prompts give you information about battery life, device connection and caller ID. Sound link wireless headphones are lighter and more comfortable than other comparable wireless headphones so you can enjoy them all day long. Wear them almost anywhere, and experience the uncompromised wireless performance. Included: sound link Around Ear headphones; detachable audio cable; carrying case. Ear cushion: 3.8 inches, Height x 2.9 inches width.",
+    rating:"4.6/5",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71jDdUuRi8L._AC_SL1500_.jpg",
+    link:
+      "https://www.amazon.com/Bose-SoundLink-around-ear-wireless-headphones/dp/B0117RGG8E/ref=sr_1_8?crid=33YKGPOX1Z35R&dchild=1&keywords=headphones&qid=1614152319&refinements=p_n_feature_two_browse-bin%3A509316&rnid=2266979011&s=aht&sprefix=headp%2Caps%2C335&sr=1-8"
+  },
+  {
+    id: 8,
+    name: "Bose Sport Earbuds",
+    price: "$179.00",
+    description:
+      "Bose Sport Earbuds are designed to energize your exercise with acclaimed lifelike sound and a comfortable secure fit. Get ready to beat your personal best, again and again.",
+    rating:"4.5/5",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61k5%2Bdpy0yL._AC_SL1500_.jpg",
+    link:
+      "https://www.amazon.com/Bose-Sport-Earbuds-Earphones-Headphones/dp/B08CJCTG6Z/ref=sr_1_11?crid=33YKGPOX1Z35R&dchild=1&keywords=headphones&qid=1614152319&refinements=p_n_feature_two_browse-bin%3A509316&rnid=2266979011&s=aht&sprefix=headp%2Caps%2C335&sr=1-11"
+  },
+  {
+    id: 9,
+    name: "Bose SoundSport",
+    price: "$129.00",
+    description:
+      "With no wires in the way, Bose sound sport wireless headphones keep you moving with powerful audio and Stay Hear+ tips designed for comfort and stability.",
+    rating:"4.5/5",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61aca1w7aDL._AC_SL1500_.jpg",
+    link:
+      "https://www.amazon.com/Bose-SoundSport-Wireless-Headphones-761529-0010/dp/B01L7PSJFO/ref=sr_1_13?crid=33YKGPOX1Z35R&dchild=1&keywords=headphones&qid=1614152319&refinements=p_n_feature_two_browse-bin%3A509316&rnid=2266979011&s=aht&sprefix=headp%2Caps%2C335&sr=1-13"
+  },
+  {
+    id: 10,
+    name: "Sony Wireless Headphones WH-CH510:",
+    price: "$58.00",
+    description:
+      "Get dynamic sound and all-day listening in a wireless headphone The zero-compromise wh-ch510 wireless headphones feature an extra-long battery life of up to 35 hours 30mm drivers bring crisp clear sound No matter what you’re listening to Make hands-free calls with the built-in microphone and recharge with the updated USB-C connection.",
+    rating:"4.5/5",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/811u6QEwQxL._AC_SL1500_.jpg",
+    link:
+      "https://www.amazon.com/Sony-WH-CH510-Wireless-Headphones-WHCH510/dp/B07WSKKYPR/ref=sr_1_12?crid=33YKGPOX1Z35R&dchild=1&keywords=headphones&qid=1614152319&refinements=p_n_feature_two_browse-bin%3A509316&rnid=2266979011&s=aht&sprefix=headp%2Caps%2C335&sr=1-12"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
