1143be0a8e273ff6a8f0982024dc7e91667e4574
askhat-king <78610124+askhat-king@users.noreply.github.com>
2021-03-03 16:28:41 +0600

    Add files via upload

---
+++ lab5/src/app/products.ts
@@ -0,0 +1,305 @@
+export const products = [
+  {
+    category_id: 0,
+    category_name: "Headsets",
+    display: false,
+    items: [
+      {
+        id: 0,
+        name: "Microsoft Surface Headphones 2 - Light Gray",
+        price: "$245.36",
+        description:
+          "Experience your favorite music and shows like never before. Designed for all-day comfort, improved sound quality, and more battery life, Surface Headphones 2 features voice, touch and fingertip controls with 13 levels of active noise cancellation.",
+        rating: "4.5/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/61v5xP47SbL._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/dp/B086Q9YGD3/ref=twister_B088B22VLB?_encoding=UTF8&psc=1",
+        likes: 50
+      },
+      {
+        id: 1,
+        name: "Sony WH-1000XM4",
+        price: "$348.00",
+        description:
+          "Sony’s intelligent industry-leading noise canceling headphones with premium sound elevate your listening experience with the ability to personalize and control everything you hear.",
+        rating: "4.7/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/71o8Q5XJS5L._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/Sony-WH-1000XM4-Canceling-Headphones-phone-call/dp/B0863TXGM3/ref=sr_1_1?crid=3QSOYN34A5JAQ&dchild=1&keywords=sony+mx4&qid=1614148885&sprefix=sony+mx%2Caps%2C348&sr=8-1",
+        likes: 5
+      },
+      {
+        id: 2,
+        name: "Beats Solo Pro",
+        price: "$299.95",
+        description:
+          "Experience powerful, balanced sound with Solo Pro wireless on-ear headphones featuring Active Noise Cancelling",
+        rating: "4.7/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/61w%2BedOrp9L._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/Beats-Wireless-Noise-Cancelling-Headphones-Bluetooth/dp/B07YVYZLC8/ref=sr_1_8?dchild=1&keywords=beats%2Bby%2Bdre&qid=1614152051&sr=8-8&th=1",
+        likes: 10
+      },
+      {
+        id: 3,
+        name: "Bose QuietComfort 35 II",
+        price: "$299.00",
+        description:
+          "QuietComfort 35 wireless headphones II are engineered with renowned noise cancellation. With the Google Assistant and Amazon Alexa built-in, you have instant access to millions of songs, playlists and more—hands free.* Simply choose your voice assistant and ask away.",
+        rating: "4.7/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/81%2BjNVOUsJL._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/Bose-QuietComfort-Wireless-Headphones-Cancelling/dp/B0756CYWWD/ref=sr_1_3?crid=33YKGPOX1Z35R&dchild=1&keywords=headphones&qid=1614152319&refinements=p_n_feature_two_browse-bin%3A509316&rnid=2266979011&s=aht&sprefix=headp%2Caps%2C335&sr=1-3",
+        likes: 4
+      },
+      {
+        id: 4,
+        name: "Sony Wireless Headphones WH-CH510:",
+        price: "$58.00",
+        description:
+          "Get dynamic sound and all-day listening in a wireless headphone The zero-compromise wh-ch510 wireless headphones feature an extra-long battery life of up to 35 hours 30mm drivers bring crisp clear sound No matter what you’re listening to Make hands-free calls with the built-in microphone and recharge with the updated USB-C connection.",
+        rating: "4.5/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/811u6QEwQxL._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/Sony-WH-CH510-Wireless-Headphones-WHCH510/dp/B07WSKKYPR/ref=sr_1_12?crid=33YKGPOX1Z35R&dchild=1&keywords=headphones&qid=1614152319&refinements=p_n_feature_two_browse-bin%3A509316&rnid=2266979011&s=aht&sprefix=headp%2Caps%2C335&sr=1-12",
+        likes: 11
+      }
+    ]
+  },
+  {
+    category_id: 1,
+    category_name: "Monitors",
+    display: false,
+    items: [
+      {
+        id: 0,
+        name: "BenQ MOBIUZ EX2710 27 Inch 144Hz IPS Gaming Monitor",
+        price: "$299.99",
+        description:
+          "EX2710 features HDRi that optimizes color performance, detail contrast, and image clarity add intensity so games look their best and you see every strategic detail. Its bezel-less design opens screen space and eliminates distracting edges that can take your eye off the action.",
+        rating: "4.6/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/61U0zYXrYhL._AC_SL1001_.jpg",
+        link:
+          "https://www.amazon.com/BenQ-EX2710-Monitor-FreeSync-Speakers/dp/B08FF8H42N/ref=sr_1_2_sspa?dchild=1&qid=1614764950&rnid=541966&s=electronics&sr=1-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzNFc3WlpHMEVMNDA1JmVuY3J5cHRlZElkPUEwODE5NDg2MVlQQTEzU0o2MFhLTSZlbmNyeXB0ZWRBZElkPUEwOTc0MDk5MVE2WVRBQ0pLUzZHSiZ3aWRnZXROYW1lPXNwX2F0Zl9icm93c2UmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl",
+        likes: 88
+      },
+      {
+        id: 1,
+        name: "Sceptre Curved 27 75Hz LED Monitor",
+        price: "$166.99",
+        description:
+          "With the C275W-1920RN, a slender 1800R screen curvature yields wide-ranging images that seemingly surround you. Protection and comfort are the hallmarks of this design as the metal pattern brush finish is smooth and pleasing to the touch. 1080P resolution (1920 x 1080 pixels) provides stunning color and picture detail on a 24 inch screen. Enjoy VGA and HDMI inputs to connect all video and gaming devices. 27 Curved monitor 1920 x 1080 Full HD resolution up to 75Hz refresh rate Fast response time HDMI, VGA & headphone ports dimensions with stand (inches) 24.27”X 9.01”X 17.69” inches without stand (inches) 24.27” x 2.56” x 14.43” packaging Dimension (inches) 27.99”X 5.43”X 18.74” inches HDMI; VGA.The brightness is 250 CD/M2. Viewing Angle: 178˚ (Horizontal) / 178˚ (Vertical).",
+        rating: "4.7/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/914W%2BOtJQ-L._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/Sceptre-Monitor-Speakers-Edge-Less-C275W-1920RN/dp/B07MTRQ6B3/ref=sr_1_8?dchild=1&qid=1614764950&rnid=541966&s=electronics&sr=1-8",
+        likes: 11
+      },
+      {
+        id: 2,
+        name:
+          "LG 27GL83A-B 27 Inch Ultragear QHD IPS 1ms NVIDIA G-SYNC Compatible Gaming Monitor, Black",
+        price: "$379.99",
+        description:
+          "The 27” Ultra Gear QHD IPS 1ms 144Hz monitor is G-Sync Compatible and has a 3-Side Virtually Borderless bezel. Other features includes: Tilt / Height / Pivot Adjustable Stand.",
+        rating: "4.8/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/91lS5yEDH-L._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/LG-27GL83A-B-Ultragear-Compatible-Monitor/dp/B07YGZL8XF/ref=sr_1_10?dchild=1&qid=1614764950&rnid=541966&s=electronics&sr=1-10",
+        likes: 102
+      },
+      {
+        id: 3,
+        name: "AOC CQ27G2",
+        price: "$259.99",
+        description:
+          "27-inch Curved Gaming Monitor. FreeSync support ensures a stutter-free and tear-free gaming experience at any frame rate Equipped with a curved 27 VA panel the CQ27G2 displays a detailed Quad HD resolution (2560x1440 pixels) at a screaming 144 Hz refresh rate and 1ms (MPRT) response time.",
+        rating: "4.7/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/81oORaahKZL._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/AOC-CQ27G2-Frameless-Adjustable-Guarantee/dp/B0862YHJ56/ref=sr_1_12?dchild=1&qid=1614764950&rnid=541966&s=electronics&sr=1-12",
+        likes: 12
+      },
+      {
+        id: 4,
+        name: "ViewSonic VP2468 Professional 24 inch 1080p Monitor ",
+        price: "$229.99",
+        description:
+          "The ViewSonic VP2468 is a 24'' (23.8” Viewable) full HD monitor that delivers unmatched Color accuracy crucial for professional applications. With an Edge-to-Edge super clear IPS panel and one of the world's thinnest bezels, the VP2468 provides a sleek and frameless viewing experience ideal for multi-monitor setups.",
+        rating: "4.5/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/61Xn5VxL5OL._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/ViewSonic-VP2468-Professional-Calibration-Photography/dp/B01HSABSGE/ref=sr_1_14_sspa?dchild=1&qid=1614764950&rnid=541966&s=electronics&sr=1-14-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzNFc3WlpHMEVMNDA1JmVuY3J5cHRlZElkPUEwODE5NDg2MVlQQTEzU0o2MFhLTSZlbmNyeXB0ZWRBZElkPUEwNzQ0MTgwMTNYMDdXSFpXRU9OViZ3aWRnZXROYW1lPXNwX210Zl9icm93c2UmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl",
+        likes: 2
+      }
+    ]
+  },
+  {
+    category_id: 2,
+    category_name: "Smart Watches",
+    display: false,
+    items: [
+      {
+        id: 0,
+        name: "AUKEY Smart Watch",
+        price: "$39.99",
+        description:
+          "Model: AUKEY-LS02 Screen: 1.4-inch TFT LCD display Resolution: 320 x 320p BT Version: BT 5 Sensors: Heart rate sensor, acceleration sensor Battery Capacity: 260mAh Battery Life: Up to 20 days Operating Temperature: -20°C – 45°C / -4°F – 113°F IP Rating: IP68 System Compatibility: Android 4.4 / iOS 8.0 or higher Data Sync Platform: AUKEY Fit app Watch Body Dimensions: 48 x 36 x 12mm / 1.89 x 1.42 x 0.47  Watch Strap Width: 20mm / 0.79 Weight (watch strap included): 38g / 1.34oz",
+        rating: "4.2/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/71wobXrkYIL._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/AUKEY-Waterproof-Pedometer-Stopwatch-Notifications/dp/B08JQL49HQ/ref=sr_1_5?crid=20Q4KVCXLZU0L&dchild=1&keywords=smart+watch&qid=1614765701&sprefix=smart%2Caps%2C348&sr=8-5",
+        likes: 8
+      },
+      {
+        id: 1,
+        name:
+          "Smart Watch, Virmee VT3 Fitness Tracker with Heart Rate Monitor Blood Oxygen Meter Sleep Tracking Step Counter",
+        price: "$45.99",
+        description:
+          "Virmee Smart Watch VT3, Health and Fitness Tracker24/7 Heart Rate Tracking .Use heart rate to better track calorie burn, optimise effort during workouts, see your resting heart rate trends and more. SpO2 & Stress Status Virmee VT3 can gauges your blood oxygen saturation and stress status(for reference only). ",
+        rating: "4.5/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/716bmsFN4hL._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/Virmee-VT3-Waterproof-Pedometer-Smartwatch/dp/B08BZ6F4H6/ref=sr_1_1_sspa?crid=20Q4KVCXLZU0L&dchild=1&keywords=smart+watch&qid=1614765701&sprefix=smart%2Caps%2C348&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyVlhTV0VPNjkzVlAwJmVuY3J5cHRlZElkPUEwODM4MjE0MUpFU0tCRTFNNEExNSZlbmNyeXB0ZWRBZElkPUEwNTA0NTQ2TUlHMUUyVjE4R1g0JndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==",
+        likes: 11
+      },
+      {
+        id: 2,
+        name:
+          "Amazfit Bip U Pro Smart Watch with Built-in GPS, 9-Day Battery Life, Fitness Tracker",
+        price: "$69.99",
+        description:
+          "Four built-in watch faces. You can replace two of the built-in watch faces from a selection of 40+ online watch faces in the Zepp App.",
+        rating: "4.1/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/61brDD8JILL._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/Amazfit-Battery-Fitness-Tracker-Resistant/dp/B08JRZS7K3/ref=sr_1_2_sspa?crid=20Q4KVCXLZU0L&dchild=1&keywords=smart+watch&qid=1614765701&sprefix=smart%2Caps%2C348&sr=8-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyVlhTV0VPNjkzVlAwJmVuY3J5cHRlZElkPUEwODM4MjE0MUpFU0tCRTFNNEExNSZlbmNyeXB0ZWRBZElkPUEwNDI5NjgzMVBWNDlIQUpRVVg2VCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=",
+        likes: 12
+      },
+      {
+        id: 3,
+        name:
+          "Willful Smart Watch for Android Phones and iOS Phones Compatible iPhone Samsung, ",
+        price: "$35.99",
+        description:
+          "Compatible with most iOS 8.0 & Android 4.4 above smartphones. Download and install Free 'VeryFitPro' APP, bind this smartwatch in your cell phone,you can receive alerts of calendar, SMS, incoming calls and SNS (Facebook, WhatsApp, LinkedIn, Instagram, Twitter and more) on this watch;It will never let you miss any important things.",
+        rating: "4.3/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/61l%2BWYDAi1L._AC_SL1300_.jpg",
+        link:
+          "https://www.amazon.com/Willful-Compatible-Swimming-Waterproof-Smartwatch/dp/B08H23ZYDL/ref=sr_1_3?crid=20Q4KVCXLZU0L&dchild=1&keywords=smart%2Bwatch&qid=1614765701&sprefix=smart%2Caps%2C348&sr=8-3&th=1",
+        likes: 12
+      },
+      {
+        id: 4,
+        name:
+          "Letsfit Smart Watch for Android Phones Compatible with iPhone Samsung",
+        price: "$39.99",
+        description:
+          "All-day Activity Tracking: Our Letsfit activity tracker can easily monitor your steps, heart rate, distance travelled and more with 14 sports modes and GPS tracking that can be used when you connect your fitness watch to our Letsfit app.",
+        rating: "4.3/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/61Cc0WJPkZL._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/Letsfit-Android-Compatible-Saturation-Waterproof/dp/B08R9P1D3H/ref=sr_1_4?crid=20Q4KVCXLZU0L&dchild=1&keywords=smart+watch&qid=1614765701&sprefix=smart%2Caps%2C348&sr=8-4",
+        likes: 2
+      }
+    ]
+  },
+  {
+    category_id: 3,
+    category_name: "Speakers",
+    display: false,
+    items: [
+      {
+        id: 0,
+        name:
+          "Klipsch R-52C Powerful Detailed Center Channel Home Speaker - Black",
+        price: "$199.00",
+        description:
+          "There is no more important speaker to your surround sound system than your center channel. It’s the speaker that holds the rest of the system together. All of your dialogue comes from this speaker and that means you want to have the best quality speaker you can find to get the clearest experience possible. If you can’t understand the words, the rest is irrelevant.",
+        rating: "4.9/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/81sbro%2BABiL._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/Klipsch-R-52C-Powerful-Detailed-Channel/dp/B07FK41FGL/ref=sr_1_9?dchild=1&qid=1614766663&rnid=667846011&s=electronics&sr=1-9",
+        likes: 88
+      },
+      {
+        id: 1,
+        name:
+          "Klipsch R-41M Powerful Detailed Bookshelf Home Speaker Set of 2 Black",
+        price: "$109.99",
+        description:
+          "The R-41M bookshelf speakers offer maximum versatility without sacrificing performance from a small, yet elegant audio solution. Designed as main speakers for a compact living space or surround sound to support a Reference speaker system, the R-41M bookshelf speakers are the ideal choice for your home theater system.",
+        rating: "4.7/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/514tHVHYEtL._AC_SL1000_.jpg",
+        link:
+          "https://www.amazon.com/Klipsch-R-41M-Powerful-Detailed-Bookshelf/dp/B07FKH3VPV/ref=sr_1_5?dchild=1&qid=1614766663&rnid=667846011&s=electronics&sr=1-5",
+        likes: 11
+      },
+      {
+        id: 2,
+        name:
+          "Sonos Sub (Gen 3) - The Wireless Subwoofer for Deep Bass - Black",
+        price: "$699.00",
+        description:
+          "Hear and feel the difference with Sub, the wireless subwoofer for deep bass. Experience lower lows and higher highs when you connect Sub to your Sonos system over WiFi. Place the award-winning design upright or lay it on its side and enjoy pure bass with no vibration or rattle.",
+        rating: "4.8/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/61tuTVlluFL._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/Sonos-Sub-Gen-Wireless-Subwoofer/dp/B087CCZH4Q/ref=sr_1_6?dchild=1&qid=1614766663&rnid=667846011&s=electronics&sr=1-6",
+        likes: 102
+      },
+      {
+        id: 3,
+        name:
+          "Edifier R1280T Powered Bookshelf Speakers - 2.0 Stereo Active Near Field Monitors - Studio Monitor Speaker - Wooden Enclosure - 42 Watts RMS",
+        price: "$99.99",
+        description:
+          "The R1280T powered bookshelf speakers has combined a retro look with a modern sense of audio quality. If you’re looking for affordable and versatile bookshelf speakers with uncompromising sound the R1280T bookshelf speakers is your go-to 2.0 speaker system. Featuring a 4-inch bass driver and a calibrated, flared bass reflex port, this bookshelf speaker system delivers robust, rich bass notes that are full of presence and depth. The faithful sound production and performance of the R1280T are any audiophiles dream. Most Edifier customers are using our R1280T powered bookshelf speakers for connecting their turntables for the best audio experience. Enjoy your classic vinyl’s while enjoying the advancements in modern day audio technology.",
+        rating: "4.7/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/71o5w0ZfptL._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/Edifier-R1280T-Powered-Bookshelf-Speakers/dp/B016P9HJIA/ref=sr_1_4?dchild=1&qid=1614766663&rnid=667846011&s=electronics&sr=1-4",
+        likes: 12
+      },
+      {
+        id: 4,
+        name: "Klipsch Synergy Black Label Sub-100 Subwoofer ",
+        price: "$279.95",
+        description:
+          "An updated grille, along with the Black and copper accents you’ve come to identify as the Klipsch trademark colors, create a sleek, sophisticated look for this dynamic sub. Place it anywhere in your home and prepare for bone-rattling bass.",
+        rating: "4.7/5",
+        img:
+          "https://images-na.ssl-images-amazon.com/images/I/81M%2B6Mp6NQL._AC_SL1500_.jpg",
+        link:
+          "https://www.amazon.com/Klipsch-Synergy-Black-Sub-100-Subwoofer/dp/B07VR24V4V/ref=sr_1_10?dchild=1&qid=1614766663&rnid=667846011&s=electronics&sr=1-10",
+        likes: 2
+      }
+    ]
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
